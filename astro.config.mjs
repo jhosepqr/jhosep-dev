798aa54ae7049b2941d3dcb11d52d289f398019a
jhosepqr <jhosepqr@users.noreply.github.com>
2026-07-07 23:31:56 -0300

    ci: update site domain to jhosep.nikao.tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nikao.tech',
+  site: 'https://jhosep.nikao.tech',
   integrations: [react()]
 });
